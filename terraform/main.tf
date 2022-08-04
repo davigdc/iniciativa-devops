620f26ced4412f3d9a329f1cba8139ac9959a593
davigdc <davigdc10@gmail.com>
2022-08-04 01:26:27 -0300

    terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,6 +32,6 @@ output "kubeconfig" {
 }
 
 resource "local_file" "kube_config" {
-  content = digitalocean_kubernetes_cluster.k8s_iniciativa.kube_config.0.raw_config
+  content  = digitalocean_kubernetes_cluster.k8s_iniciativa.kube_config.0.raw_config
   filename = "kube_config.yaml"
 }
